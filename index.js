ed1a6546441754f04fa07b07db5bacd0d0490b11
Elliot Rodriguez <elliotrodriguez72@gmail.com>
2017-03-17 08:24:09 -0400

    Removed some superfluous tests, unnecessary argument for toText function

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ var zeroFill = require('zero-fill');
 module.exports = {
     toBinary: function (strToConvert, addWhiteSpace) {
         var binaryValuePreFill = '', binaryValue = '', strPosition;
-        if (strToConvert.length === 0) {
-            return null;
-        }
         for (strPosition = 0; strPosition < strToConvert.length; strPosition += 1) {
             binaryValuePreFill = strToConvert.charCodeAt(strPosition).toString(2);
             binaryValue += zeroFill(8, binaryValuePreFill);
@@ -15,30 +12,27 @@ module.exports = {
         }
         return binaryValue.trim();
     },
-    toText: function (strBinaryToConvert, hasWhiteSpace) {
-        var intAsciiNumberInBaseTen = 0, binaryNumbersArray, intArrayCounter, stringToReturn = '';
-        if (hasWhiteSpace) {
+    toText: function (strBinaryToConvert) {
+        var intAsciiNumberInBaseTen = 0,
+            binaryNumbersArray = [],
+            intArrayCounter,
+            intStrPos,
+            subStrItem,
+            stringToReturn = '';
+        if (strBinaryToConvert.indexOf(' ') > 0) {
             binaryNumbersArray = strBinaryToConvert.split(' ');
-            if (binaryNumbersArray.length > 0) {
-                for (intArrayCounter = 0; intArrayCounter !== binaryNumbersArray.length; intArrayCounter += 1) {
-                    intAsciiNumberInBaseTen = parseInt(binaryNumbersArray[intArrayCounter], 2);
-                    stringToReturn += String.fromCharCode(intAsciiNumberInBaseTen);
-                }
+        } else {
+            for (intStrPos = 0; intStrPos !== strBinaryToConvert.length; intStrPos += 8) {
+                subStrItem = strBinaryToConvert.substr(intStrPos, 8);
+                binaryNumbersArray.push(subStrItem);
             }
-	} else {
-	// split every 8 characters
-		var strBinLengthWithoutSpaces = strBinaryToConvert.length;
-		var binLengthOffset = strBinLengthWithoutSpaces % 8;
-		
-		for(var i = 0; i != strBinLengthWithoutSpaces; i += 8) {
-			var subStrItem = strBinaryToConvert.substr(i,8);
-			binaryNumbersArray.push(subStrItem);
-		}
-		
-	}
-
+        }
+        if (binaryNumbersArray.length > 0) {
+            for (intArrayCounter = 0; intArrayCounter !== binaryNumbersArray.length; intArrayCounter += 1) {
+                intAsciiNumberInBaseTen = parseInt(binaryNumbersArray[intArrayCounter], 2);
+                stringToReturn += String.fromCharCode(intAsciiNumberInBaseTen);
+            }
+        }
         return stringToReturn;
     }
-}strBinaryToConvert.substr(i, i + 7);
-			binaryNumbersArray.push(subBinaryItem);
-			return;
+};
--- test/index.js
+++ test/index.js
@@ -12,11 +12,19 @@ describe('#toBinary', function() {
         toBinary('Hello World!', false).should.equal('010010000110010101101100011011000110111100100000010101110110111101110010011011000110010000100001');
     });
 
-    it ('converts a single binary value to a single ASCII character regardless of spacing', function() {
+    it('converts a string to binary if no spaces are present', function() {
+        toBinary('Hello', false).should.equal('0100100001100101011011000110110001101111');
+    })
+
+    it ('converts a single binary value to a single ASCII character regardless of spacing argument', function() {
         toText('01001000', true).should.equal('H');
     });
 
      it ('converts a single binary value with spaces to a single ASCII character with spaces', function() {
-        toText('01001000 01100101 01101100 01101100 01101111 00100000 01010111 01101111 01110010 01101100 01100100 00100001', true).should.equal('Hello World!');
+        toText('01001000 01100101 01101100 01101100 01101111 00100000 01010111 01101111 01110010 01101100 01100100 00100001').should.equal('Hello World!');
+    });
+
+    it('converts a binary string without spaces to ASCII characters', function() {
+        toText('010010000110010101101100011011000110111100100000010101110110111101110010011011000110010000100001').should.equal('Hello World!');
     });
 })    
